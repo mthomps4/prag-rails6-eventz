8612e31c97eab471bc11d6e7bdf2d46a6dc5bc99
Matt Thompson <matt@echobind.com>
2020-04-02 17:04:39 -0400

    asdf

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+  def free?
+    price.zero? || price.blank?
+  end
+
+  def thing
+    "asdf"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_142538) do
-
+ActiveRecord::Schema.define(version: 2020_04_02_141804) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "starts_at"
+    t.text "description"
   end
-
 end
